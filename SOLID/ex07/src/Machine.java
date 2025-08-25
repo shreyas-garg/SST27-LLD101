204d9ef3cd5e7df27dc2c6837ebf3e6c846963b8
Shreyas-hub-coder <shreyas.garg2005@gmail.com>
2025-08-25 10:36:53 +0530

    ex07

--- SOLID/ex07/src/Machine.java
+++ SOLID/ex07/src/Machine.java
@@ -1,5 +1,11 @@
-interface Machine {
+interface Printer {
     void print(String text);
+}
+
+interface Scanner {
     void scan(String dstPath);
+}
+
+interface Fax {
     void fax(String number);
 }

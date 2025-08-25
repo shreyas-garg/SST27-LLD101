ded3003f2fe9b8835268ca63cfca815e7f6b0711
Shreyas-hub-coder <shreyas.garg2005@gmail.com>
2025-08-25 11:26:20 +0530

    ex 05 03

--- SOLID/ex03/src/Demo03.java
+++ SOLID/ex03/src/Demo03.java
@@ -2,7 +2,5 @@ public class Demo03 {
     public static void main(String[] args) {
         ShippingCostCalculator calculator = new ShippingCostCalculator();
         System.out.println(calculator.cost(new Shipment("EXPRESS", 2.0)));
-        System.out.println(calculator.cost(new Shipment("STANDARD", 3.0)));
-        System.out.println(calculator.cost(new Shipment("OVERNIGHT", 1.5)));
     }
 }
